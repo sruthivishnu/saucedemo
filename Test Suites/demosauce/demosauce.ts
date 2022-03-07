fbaac606d3995c4f9d19d01fcb87bcb3377f49d6
win <win@DESKTOP-SSQKLGL>
2022-03-07 22:46:33 +0530

    selfstudy

---
+++ Test Suites/demosauce/demosauce.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>demosauce</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>46d22c19-4fbb-488b-b39e-36648e724075</testSuiteGuid>
+</TestSuiteEntity>
